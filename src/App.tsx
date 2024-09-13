e1e95aa770ce65c7be55bd1a341450462a7afb42
TylpinMO <113288606+TylpinMO@users.noreply.github.com>
2024-09-13 08:59:18 +0300

    initial text

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,10 @@ const App = () => {
 					</div>
 					<div className='text-base mt-2 flex items-center'>
 						<img src={trophy} width={24} height={24} />
-						<span className='ml-1'>STON.fi ✖ The Open League</span>
+						<span className='ml-1'>
+							STON.fi ✖ The Open League
+							<Arrow size={18} className='ml-0 mb-1 inline-block' />
+						</span>
 					</div>
 				</div>
 
