d05dfdd619259536fd4576a80378f5b64a18d517
MishenkovDaniil <mishenkovdaniil@mail.ru>
2024-09-13 08:52:27 +0300

    add label

--- src/App.tsx
+++ src/App.tsx
@@ -133,10 +133,8 @@ const App = () => {
 					</div>
 					<div className='text-base mt-2 flex items-center'>
 						{/* <img src={trophy} width={24} height={24} /> */}
-						<span className='ml-1'>
 						<span>
-							STON.fi ✖ The Open League
-						</span> 
+						STON.fi ✖ The Open League
 						{/* <Arrow size={18} className='ml-0 mb-1 inline-block' /> */}
 						</span>
 					</div>
